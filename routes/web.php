94bad400f4b5b51304edb0ebb2be3e1fafd96d65
maryam khodaparast <khcomputergirl@gmail.com>
2020-09-17 17:50:58 +0430

    Inserting Data 17-117

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
+use App\Post;
+use App\Video;
+use App\Tag;
+
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/create', function () {
+    $post = Post::create(['name' => 'My First Post']);
+    $tag1 = Tag::findOrFail(1);
+    $post->tags()->save($tag1);
+    $video = Video::create(['name' => 'video.mov']);
+    $tag2 = Tag::findOrFail(2);
+    $video->tags()->save($tag2);
 });
